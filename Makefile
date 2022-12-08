6976c071026cd82dc6adc3327e6b32aad040adc8
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-12-08 18:45:56 +0900

    make credentials

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 .PHONY: all
 all:
-	python -m gcal_sync \
+	python -m gcal_sync run \
 		credentials/gcal_sync-jdsc.json \
 		credentials/gcal_sync-hotoku.json \
 		primary \
 		3427b56b797a40bfe4a664440ac4116972f521d8b436fcc8c6776a2619f55e40@group.calendar.google.com
+
+
+.PHONY: credentials
+credentials:
+	python -m gcal_sync credentials \
+		credentials/gcal_sync-jdsc.json \
+		credentials/gcal_sync-hotoku.json
 
 
 .PHONY: clean
--- gcal_sync/__main__.py
+++ gcal_sync/__main__.py
@@ -43,19 +43,28 @@ def get_credentials(json_path: str, name: str) -> Credentials:
 
     if creds is not None and creds.expired and creds.refresh_token:
         creds.refresh(Request())
-    else:
-        LOGGER.error("credential for %s is not valid", name)
-        raise RuntimeError("credential for %s is not valid" % name)
-        print(name)
-        flow = InstalledAppFlow.from_client_secrets_file(
-            json_path, scopes)
-        creds = flow.run_local_server(port=0)
-        if not isinstance(creds, Credentials):
-            raise RuntimeError("unknown result from run_local_server")
+        with open(token_path, 'w') as token:
+            token.write(creds.to_json())
 
+    else:
+        raise RuntimeError("credential for %s is not valid" % name)
+
+    return creds
+
+
+def make_token(json_path: str, name: str):
+    scopes = ['https://www.googleapis.com/auth/calendar']
+
+    print(f"start making token for {name}")
+    flow = InstalledAppFlow.from_client_secrets_file(
+        json_path, scopes)
+    creds = flow.run_local_server(port=0)
+    if not isinstance(creds, Credentials):
+        raise RuntimeError("unknown result from run_local_server")
+
+    token_path = secret2token(json_path)
     with open(token_path, 'w') as token:
         token.write(creds.to_json())
-    return creds
 
 
 def get_service(creds: Credentials):
@@ -131,7 +140,12 @@ def create_events(creds: Credentials, events: list[dict[str, Any]], calendar_id:
     batch.execute()
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
 @click.argument("from_json", type=click.Path(exists=True, dir_okay=False))
 @click.argument("to_json", type=click.Path(exists=True, dir_okay=False))
 @click.argument("from_id", type=str)
@@ -142,16 +156,24 @@ def create_events(creds: Credentials, events: list[dict[str, Any]], calendar_id:
 @click.option("--duration", type=int,
               default=30,
               help="duration of synchronized interval in days. default to 30.")
-def main(from_json: str, to_json: str, from_id: str, to_id: str,
-         start_time: datetime, duration: int):
+def run(from_json: str, to_json: str, from_id: str, to_id: str,
+        start_time: datetime, duration: int):
     from_creds = get_credentials(
-        from_json, "start authenticate process for FROM calendar")
+        from_json, "FROM")
     to_creds = get_credentials(
-        to_json, "start authenticate process for TO calendar")
+        to_json, "TO")
 
     delete_events(to_creds, start_time, duration, to_id)
     events = list_events(from_creds, start_time, duration, from_id)
     create_events(to_creds, events, to_id)
+
+
+@main.command()
+@click.argument("from_json", type=click.Path(exists=True, dir_okay=False))
+@click.argument("to_json", type=click.Path(exists=True, dir_okay=False))
+def credentials(from_json: str, to_json: str):
+    make_token(from_json, "FROM")
+    make_token(to_json, "TO")
 
 
 if __name__ == '__main__':
