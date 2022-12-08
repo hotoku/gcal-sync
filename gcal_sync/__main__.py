3ce2ae65b39c36a894b121e482cde66d200b382a
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-12-08 15:24:46 +0900

    get events from from and to calendar

--- gcal_sync/__main__.py
+++ gcal_sync/__main__.py
@@ -1,13 +1,11 @@
 from __future__ import print_function
 
-import datetime
 import os
-from os.path import exists
+import re
 from pprint import pprint
 from datetime import datetime
 
 import click
-from google.auth import default
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -18,7 +16,6 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-TOKEN_PATH = os.path.join(os.getcwd(), "credentials/token.json")
 CLIENT_KEY_PATH = os.path.join(os.getcwd(), "credentials/gcal_sync.json")
 
 
@@ -48,7 +45,7 @@ def print_calendar_example():
         service = build('calendar', 'v3', credentials=creds)
 
         # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
         print('Getting the upcoming 10 events')
         events_result = service.events().list(calendarId='primary', timeMin=now,
                                               maxResults=10, singleEvents=True,
@@ -66,13 +63,58 @@ def print_calendar_example():
         print('An error occurred: %s' % error)
 
 
+def secret2token(json_path: str) -> str:
+    dir_name = os.path.dirname(json_path)
+    base_name = os.path.basename(json_path)
+    body = re.sub(r"\.[^\.]+$", "", base_name)
+    return os.path.join(dir_name, body + "-token.json")
+
+
 def get_credentials(json_path: str) -> Credentials:
-    pass
+    token_path = secret2token(json_path)
+    creds = None
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+
+    if creds is not None and creds.valid:
+        return creds
+
+    if creds is not None and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            json_path, SCOPES)
+        creds = flow.run_local_server(port=0)
+        if not isinstance(creds, Credentials):
+            raise RuntimeError("unknown result from run_local_server")
+
+    with open(token_path, 'w') as token:
+        token.write(creds.to_json())
+    return creds
+
+
+def list_events(creds: Credentials):
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+        return
+
+    pprint([(e["id"], e["start"]["dateTime"])
+           for e in events])
 
 
 @click.command()
-@click.argument("from_json", type=click.File())
-@click.argument("to_json", type=click.File())
+@click.argument("from_json", type=click.Path(exists=True, dir_okay=False))
+@click.argument("to_json", type=click.Path(exists=True, dir_okay=False))
 @click.option("--start_time", type=click.DateTime(),
               default=datetime.now(),
               help="start time of synchronized interval. default to current time.")
@@ -82,6 +124,9 @@ def get_credentials(json_path: str) -> Credentials:
 def main(from_json: str, to_json: str, start_time: datetime, duration: int):
     from_creds = get_credentials(from_json)
     to_creds = get_credentials(to_json)
+
+    list_events(from_creds)
+    list_events(to_creds)
 
 
 if __name__ == '__main__':
