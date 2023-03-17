93e2ab2118569cbf515f821f8ec1e51dbbc1e6f1
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-03-18 05:17:37 +0900

    add inctore

--- test.mk
+++ test.mk
@@ -3,6 +3,7 @@ CRED_DIR := credentials
 
 JDSC_CAL_ID := c_4c069b0468419e2aa0ead1d9fef823021cf4f406d81f84ea931f121481cb38c1@group.calendar.google.com
 ME_CAL_ID := a6d4b4d0b6d994a56a9b23e5a30e7d1fd12dcace3f3899a03be768f4894a1be1@group.calendar.google.com
+INCTORE_CAL_ID := c_83968eb2553fe7c812e95f2ab335b496965c76a984471d7ce72008e33199551d@group.calendar.google.com
 
 
 DURATION := 60
@@ -14,7 +15,8 @@ all:
 		--duration=$(DURATION) \
 		$(CRED_DIR) \
 ME:$(ME_CAL_ID):google,\
-JDSC:$(JDSC_CAL_ID):google
+JDSC:$(JDSC_CAL_ID):google,\
+INCTORE:$(INCTORE_CAL_ID):google
 
 
 .PHONY: clear
@@ -23,7 +25,8 @@ clear:
 		--duration=$(DURATION) \
 		$(CRED_DIR) \
 ME:$(ME_CAL_ID):google,\
-JDSC:$(JDSC_CAL_ID):google
+JDSC:$(JDSC_CAL_ID):google,\
+INCTORE:$(INCTORE_CAL_ID):google
 
 
 .PHONY: credentials
@@ -31,7 +34,7 @@ credentials:
 	python -m gcal_sync $(CRED_DIR) \
 		$(CRED_DIR)/gcal-sync-001.json \
 		$(CRED_DIR) \
-		JDSC:google,ME:google
+		JDSC:google,ME:google,INCTORE:google
 
 
 .PHONY: clean
