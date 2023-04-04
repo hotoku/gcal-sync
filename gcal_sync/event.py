9c1394d5ba905bb635fd24c0f58c2b36fc94d57a
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-04-05 06:55:22 +0900

    calendar

--- gcal_sync/event.py
+++ gcal_sync/event.py
@@ -1,8 +1,18 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod, abstractproperty
 from datetime import datetime
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .calendar import Calendar
+
 
 class Event(ABC):
+    def __init__(self, cal: Calendar) -> None:
+        super().__init__()
+        self.calendar = cal
+
     @abstractmethod
     def marked(self) -> bool:
         return NotImplemented
