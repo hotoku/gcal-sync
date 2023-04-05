e22f43cbb0571fb7dbdfc91b5641ae5efe1bbda4
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-04-05 15:35:27 +0900

    done, not tested

--- gcal_sync/gcp/event_util.py
+++ gcal_sync/gcp/event_util.py
@@ -1,10 +1,21 @@
 HEADER = "#### "
 SEP = ":"
+LINE_SEPARATE = "M5FdxitkkbomCWWFkhcHBstCu78ua7"
+
+
+def search(ss: list[str], s: str) -> int:
+    try:
+        return ss.index(s)
+    except ValueError:
+        return -1
 
 
 def parse_description(s: str) -> dict[str, str]:
+    contents = s.split("\n")
+    idx = search(contents, LINE_SEPARATE)
+    ss = contents[idx + 1:]
     n = len(HEADER)
-    lines = [l[n:] for l in s.split("\n") if l[:n] == HEADER]
+    lines = [l[n:] for l in ss if l[:n] == HEADER]
     ret: dict[str, str] = {}
     for line in lines:
         try:
@@ -16,8 +27,17 @@ def parse_description(s: str) -> dict[str, str]:
     return ret
 
 
-def dump_description(d: dict[str, str]) -> str:
-    return "\n".join([
+def dump_description(d: dict[str, str], orig_desc: str | None = None) -> str:
+    orig = (
+        orig_desc +
+        "\n\n" +
+        LINE_SEPARATE +
+        "\n"
+        if orig_desc is not None
+        else ""
+    )
+    content = "\n".join([
         f"{HEADER}{k}{SEP}{v}"
         for k, v in d.items()
     ])
+    return orig + content
